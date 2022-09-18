d38b72e39c7885ac353ddca24e1ac1c033c2f74d
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-18 00:06:18 -0300

    delete sample components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
-import { Typography } from "src/components/atoms";
 import StackBadge from "@atoms/StackBadge";
 
 export default function Home() {
   return (
     <div>
-      <Typography variant="h1">Hello World!</Typography>
       <StackBadge name="teste" />
     </div>
   );
