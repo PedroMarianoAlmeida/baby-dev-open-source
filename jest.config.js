0bba9df81747ac82eb0dd1a332515a9ed1dd3722
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-23 11:56:11 -0300

    tentativa de funcionar do stackoverflow... mas nada =(

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,8 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "@atoms/(.*)": "<rootDir>/src/components/atoms/$1",
+  },
 };
 module.exports = createJestConfig(customJestConfig);
