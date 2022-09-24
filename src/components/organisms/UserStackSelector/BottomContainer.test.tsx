2d83aa134dd3fb022a0598ef926f5bc6de4ac217
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-23 22:59:48 -0300

    BottomContainer initial tests implemented

--- src/components/organisms/UserStackSelector/BottomContainer.test.tsx
+++ src/components/organisms/UserStackSelector/BottomContainer.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import BottomContainer from "./BottomContainer";
 import "@testing-library/jest-dom";
 
 describe("organism > UserStackSelector > BottomContainer", () => {
-  it("show stack group and options when showOptions is true - not working yet (and breaking everything)", () => {
+  it("show stack group and options when showOptions is true", () => {
     render(
       <BottomContainer
         showOptions
@@ -16,11 +16,27 @@ describe("organism > UserStackSelector > BottomContainer", () => {
     );
 
     expect(screen.getByText("Stack Group")).toBeInTheDocument();
-    // expect(screen.getByText("stack1")).toBeInTheDocument();
-    // expect(screen.getByText("stack2")).toBeInTheDocument();
+    expect(screen.getByText("stack1")).toBeInTheDocument();
+    expect(screen.getByText("stack2")).toBeInTheDocument();
   });
 
-  it("don't show stack group and options when showOptions is false - not working yet", () => {
+  it.skip("isSelected property is assign correctly", () => {
+    render(
+      <BottomContainer
+        showOptions
+        setShowOptions={() => {}}
+        selected={["stack1"]}
+        addSelected={() => {}}
+        removeSelected={() => {}}
+        options={[{ name: "Stack Group", stack: ["stack1", "stack2"] }]}
+      />
+    );
+
+    expect(screen.getByText("stack1")).toHaveAttribute("isSelected", true);
+    expect(screen.getByText("stack2")).toHaveAttribute("isSelected", false);
+  });
+
+  it("don't show stack group and options when showOptions is false", () => {
     render(
       <BottomContainer
         showOptions={false}
@@ -33,5 +49,39 @@ describe("organism > UserStackSelector > BottomContainer", () => {
     );
 
     expect(screen.queryByText("Stack Group")).not.toBeInTheDocument();
+  });
+
+  it("it trigger addSelected after click in a not selected option", () => {
+    const addSelected = jest.fn();
+    render(
+      <BottomContainer
+        showOptions
+        setShowOptions={() => {}}
+        selected={[]}
+        addSelected={addSelected}
+        removeSelected={() => {}}
+        options={[{ name: "Stack Group", stack: ["stack1", "stack2"] }]}
+      />
+    );
+
+    fireEvent.click(screen.getByText("stack1"));
+    expect(addSelected).toBeCalledWith("stack1");
+  });
+
+  it("it trigger removeSelected after click in a selected option", () => {
+    const removeSelected = jest.fn();
+    render(
+      <BottomContainer
+        showOptions
+        setShowOptions={() => {}}
+        selected={["stack1"]}
+        addSelected={() => {}}
+        removeSelected={removeSelected}
+        options={[{ name: "Stack Group", stack: ["stack1", "stack2"] }]}
+      />
+    );
+
+    fireEvent.click(screen.getByText("stack1"));
+    expect(removeSelected).toBeCalledWith("stack1");
   });
 });
