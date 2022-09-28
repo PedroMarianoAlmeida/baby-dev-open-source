8f315c06369812a19966a41c2ca0625a0061b66e
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-27 21:02:38 -0300

    form from documentation working

--- src/components/organisms/JobPostForm/JobPostForm.tsx
+++ src/components/organisms/JobPostForm/JobPostForm.tsx
@@ -1,9 +1,36 @@
+import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "./JobPostForm.module.css";
 
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
+
 const JobPostForm = (props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
   return (
     <div id={styles.root}>
       <p>JobPostForm</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
     </div>
   );
 };
