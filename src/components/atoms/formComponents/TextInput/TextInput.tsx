5c3ca5c6756ed29d90d8be7733c9c5d6c2516434
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-10-06 17:20:39 -0300

    solving ts errors

--- src/components/atoms/formComponents/TextInput/TextInput.tsx
+++ src/components/atoms/formComponents/TextInput/TextInput.tsx
@@ -1,20 +1,27 @@
-import React from "react";
+import { forwardRef, InputHTMLAttributes, RefObject } from "react";
 
-const TextInput = React.forwardRef((props, ref) => {
-  const { onChange, onBlur, name, errors, placeholder = name } = props;
+interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  errors: {};
+}
 
-  return (
-    <div>
-      <input
-        onChange={onChange}
-        onBlur={onBlur}
-        name={name}
-        ref={ref}
-        placeholder={placeholder}
-      />
-      <p>{errors[name]?.message}</p>
-    </div>
-  );
-});
+const TextInput = forwardRef(
+  (props: TextInputProps, ref: RefObject<HTMLInputElement>) => {
+    const { onChange, onBlur, name, errors, placeholder = name } = props;
+
+    return (
+      <div>
+        <input
+          onChange={onChange}
+          onBlur={onBlur}
+          name={name}
+          ref={ref}
+          placeholder={placeholder}
+          {...props}
+        />
+        <p>{errors[name]?.message}</p>
+      </div>
+    );
+  }
+);
 
 export default TextInput;
