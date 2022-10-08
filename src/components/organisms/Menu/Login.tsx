b622940118ab3de8da84a6b7ea8add74336d6e4c
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-10-07 21:42:13 -0300

    login logout implemented

--- src/components/organisms/Menu/Login.tsx
+++ src/components/organisms/Menu/Login.tsx
@@ -5,25 +5,18 @@ import styles from "./Menu.module.css";
 import { UserContext } from "@contexts/UserContext";
 
 const Login = () => {
-  const { user } = useContext(UserContext);
-  console.log(user);
-
-  const [isLogin, setIsLogin] = useState(false);
-
-  const handleLogin = () => setIsLogin(true);
-  const handleLogout = () => setIsLogin(false);
-
-  
+  const { currentUser, login, logout } = useContext(UserContext);
+  const { name, image } = currentUser;
 
   return (
     <LoginUI
-      isLogged={isLogin}
+      isLogged={name !== ""}
       userData={{
-        name: "Sarah",
-        image: "/sample/login-avatar.png",
+        name,
+        image,
       }}
-      handleLogin={handleLogin}
-      handleLogout={handleLogout}
+      login={login}
+      logout={logout}
     />
   );
 };
@@ -31,25 +24,26 @@ const Login = () => {
 interface LoginUiProps {
   isLogged: boolean;
   userData: { name: string; image: string };
-  handleLogin(): void;
-  handleLogout(): void;
+  login(id: string): void;
+  logout(): void;
 }
 
-const LoginUI = ({
-  isLogged,
-  userData,
-  handleLogin,
-  handleLogout,
-}: LoginUiProps) => {
+const LoginUI = ({ isLogged, userData, login, logout }: LoginUiProps) => {
   const { name, image } = userData;
-  const { menuItem, notLogged, loggedContainer, welcome, logout, avatar } =
-    styles;
+  const {
+    menuItem,
+    notLogged,
+    loggedContainer,
+    welcome,
+    logoutContainer,
+    avatar,
+  } = styles;
   return (
     <li className={menuItem}>
       {isLogged ? (
         <div id={loggedContainer}>
           <div id={welcome}>Oi, {name}</div>
-          <p id={logout} onClick={handleLogout}>
+          <p id={logoutContainer} onClick={logout}>
             Log out
           </p>
           <div id={avatar}>
@@ -57,9 +51,14 @@ const LoginUI = ({
           </div>
         </div>
       ) : (
-        <div id={notLogged} onClick={handleLogin}>
-          Login
-        </div>
+        <>
+          <div id={notLogged} onClick={() => login("2")}>
+            Login (Dev)
+          </div>
+          <div id={notLogged} onClick={() => login("1")}>
+            Login (Cur.)
+          </div>
+        </>
       )}
     </li>
   );
--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,9 +1,41 @@
-import { createContext } from "react";
+import { useState, createContext } from "react";
 
 export const UserContext = createContext();
 
+const emptyUser = {
+  id: "",
+  name: "",
+  image: "",
+  roles: [],
+};
+
 const UserProvider = (props) => {
-  return <UserContext.Provider value={{user: "testUsers"}}>{props.children}</UserContext.Provider>;
+  const [currentUser, setCurrentUser] = useState(emptyUser);
+
+  const loginService = async (id: string) => {
+    try {
+      const res = await fetch(`http://localhost:4000/users/${id}`);
+      if (res.ok) {
+        const data = await res.json();
+        return data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const login = async (id: string) => {
+    const userData = await loginService(id);
+    if (userData.name !== "") setCurrentUser(userData);
+  };
+
+  const logout = () => setCurrentUser(emptyUser);
+
+  return (
+    <UserContext.Provider value={{ currentUser, login, logout }}>
+      {props.children}
+    </UserContext.Provider>
+  );
 };
 
 export default UserProvider;
