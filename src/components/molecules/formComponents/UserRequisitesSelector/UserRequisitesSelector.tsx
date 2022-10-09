78de2f511e4acc32d2b02f3d5cadf27ede6116bd
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-10-08 22:40:34 -0300

    add requisites to search job page

--- src/components/molecules/formComponents/UserRequisitesSelector/UserRequisitesSelector.tsx
+++
@@ -1,7 +0,0 @@
-const UserRequisitesSelector = ({ requisites }) => {
-  console.log(requisites);
-
-  return <p>UserRequisitesSelector</p>;
-};
-
-export default UserRequisitesSelector;
--- src/pages/vagas.tsx
+++ src/pages/vagas.tsx
@@ -1,19 +1,69 @@
 import { useContext } from "react";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import Select from "@molecules/formComponents/Select";
 import UserStackSelector from "@organisms/UserStackSelector";
-import UserRequisitesSelector from "@molecules/formComponents/UserRequisitesSelector";
+
 import { getAllStackOptions } from "@services/stack";
 import { getRequisitesOptions } from "@services/requisites";
 import { UserContext } from "@contexts/UserContext";
+
+interface IFormInputs {
+  //stack: string[];
+  requisites: string[];
+}
+
+const schema = yup
+  .object({
+    //stack: yup.string(),
+    requisites: yup.array(),
+  })
+  .required();
 
 const JobsPage = ({ stackAllOptions, requisitesOptions }) => {
   const { currentUser } = useContext(UserContext);
   const { stackSelected } = currentUser;
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data: IFormInputs) => {
+    console.log(data);
+  };
+
+  const {
+    onChange: onChangeRequisites,
+    onBlur: onBlurRequisites,
+    name: nameRequisites,
+    ref: refRequisites,
+  } = register("requisites");
+
   return (
     <div>
       <p>Vagas</p>
-      <UserRequisitesSelector requisites={requisitesOptions} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Select
+          onChange={onChangeRequisites}
+          onBlur={onBlurRequisites}
+          name={nameRequisites}
+          ref={refRequisites}
+          errors={errors}
+          options={requisitesOptions}
+          multiple
+        />
+
+        <input type="submit" />
+      </form>
+
       <UserStackSelector
         allOptions={stackAllOptions}
         initialSelected={stackSelected}
