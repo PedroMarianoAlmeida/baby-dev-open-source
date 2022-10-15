3ba1e7425f6caad4cee67821431d14f04d006aa6
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-10-15 07:07:53 -0300

    passing id to class css duplicate in same page

--- src/components/molecules/JobCard/JobCard.tsx
+++ src/components/molecules/JobCard/JobCard.tsx
@@ -17,27 +17,30 @@ const JobCard = ({ cardData }: JobCardProps) => {
   const { companyName, companyLogo, jobTitle, jobLocation, jobStack } =
     cardData;
 
+  const { root, content, logoTitle, logo, title, name, job, place, stack } =
+    styles;
+
   return (
-    <div id={styles.root}>
+    <div className={root}>
       <Box>
-        <div id={styles.content}>
-          <div id={styles.logoTitle}>
+        <div className={content}>
+          <div className={logoTitle}>
             <Image
               src={companyLogo}
               alt={`${companyName} - ${jobTitle}`}
-              id={styles.logo}
+              className={logo}
               width={50}
               height={50}
             />
-            <div id={styles.title}>
-              <h4 id={styles.name}>{companyName}</h4>
-              <h3 id={styles.job}>{jobTitle}</h3>
+            <div className={title}>
+              <h4 className={name}>{companyName}</h4>
+              <h3 className={job}>{jobTitle}</h3>
             </div>
           </div>
-          <h5 id={styles.place}>{jobLocation}</h5>
+          <h5 className={place}>{jobLocation}</h5>
         </div>
         <hr />
-        <div id={styles.stack}>
+        <div className={stack}>
           {jobStack.map((tec) => (
             <StackBadge name={tec} key={tec} />
           ))}
