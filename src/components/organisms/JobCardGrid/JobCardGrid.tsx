b8e86b241c3e2ffa16797ad9a8bd1207194e2eb8
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-10-15 06:29:50 -0300

    render job cards

--- src/components/organisms/JobCardGrid/JobCardGrid.tsx
+++ src/components/organisms/JobCardGrid/JobCardGrid.tsx
@@ -1,7 +1,30 @@
+import JobCard from "@molecules/JobCard";
+
 const JobCardGrid = ({ jobs }) => {
   console.log("Recent Jobs - JobCardGrid->", jobs);
 
-  return <p>Job card grid</p>;
+  const jobsCardData = jobs.map((job) => {
+    const { company, title, location, stack } = job;
+    return {
+      company: {
+        logo: "",
+        name: company,
+      },
+      job: {
+        title,
+        place: location,
+      },
+      stack: stack,
+    };
+  });
+
+  return (
+    <>
+      {jobsCardData.map((job) => (
+        <JobCard cardData={job} />
+      ))}
+    </>
+  );
 };
 
 export default JobCardGrid;
